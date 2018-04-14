926807d0e0029d59a385c6f2132b45c82442e002
bassant46 <bassant46df@gmail.com>
2018-04-14 17:57:00 -0400

    ...

--- bst.hpp
+++ bst.hpp
@@ -28,69 +28,176 @@ Tree create()
 
 bool isEmpty( Tree node )
 {
-
+    return node == nullptr;
 }
 
 bool isLeaf( Tree node )
 {
+   return (node->left == nullptr) && (node->right == nullptr) ;
 
 }
 
 int size( Tree node )
 {
-
+    if (!isEmpty(node))
+    {
+        return 1+size(node->right)+size(node->left);
+    }
+    else
+        return 0;
 }
 
 bool find( Tree tree, std::string item )
 {
-
+    if (!isEmpty(tree))
+    {
+        if ( item == tree->item)
+            return true;
+        else if (item < tree->item)
+            return find(tree->left, item);     
+        else 
+            return find(tree->right, item);
+    }
+    if (isEmpty(node))
+    {
+        return false;
+    }
 }
 
 void insert( Tree &tree, std::string item )
 {
-
+    if (isEmpty(tree))
+    {
+        tree = BSTNode ( item,nullptr,nullptr);
+    }
+    else 
+    {
+        if ( item < tree-> item)
+        {
+            insert(tree->left, item);
+        }
+        else
+        {
+            insert(tree->right, item);
+        }
+    }
 }
 
 Tree minNode( Tree tree )
 {
+    if (!isEmpty(tree))
+    {
+       if (tree->left == null) {
+           return tree->item;
+       }       
+       else 
+       return minNode(tree->left);
+    }
 
 }
 
 Tree maxNode( Tree tree )
 {
+  if (!isEmpty(tree))
+    {
+       if (tree->right == null) {
+           return tree-> item;
+       }       
+       else 
+       return maxNode(tree->right);
+    }  
 
 }
 
 void remove( Tree &tree, std::string item )
 {
+    if ( isEmpty( tree )) return;
 
+    if ( item == tree->item)
+    {
+        if ( !isEmpty( tree->left ) && !isEmpty( tree->right ))
+        {
+            BSTNode *minRight = minNode( tree->right );
+            tree->item = minRight->item;
+            remove( tree->right, minRight->item );
+        } else
+        {
+            BSTNode *discard = tree;
+
+            if ( isLeaf( tree ))
+                tree = nullptr;
+            else if ( !isEmpty( tree->left ))
+                tree = tree->left;
+            else
+                tree = tree->right;
+
+            delete discard;
+        }
+
+    } else if ( item < tree->item )
+        remove( tree->left, item );
+    else remove( tree->right, item );
 }
 
 
 void clear( Tree &tree )
 {
-
+    bool (!isEmpty(tree))
+    {
+        clear(tree->left);
+        clear (tree->right);
+        delete tree;
+        tree = nullptr;
+    }
 }
 
 void preorder( Tree tree )
 {
-
+    if (tree)
+    {
+        std::cout<< item;
+        preorder(tree->left);
+        preorder(tree->right);  
+        
+    }
 }
 
 void inorder( Tree tree )
-{
-
+{   
+    if (tree)
+    {
+        inorder (tree->left);
+        std::cout<< item;
+        inorder (tree->right); 
+    }
 }
 
 void postorder( Tree tree )
 {
-
+    if (tree)
+    {
+        postorder (tree->left);
+        postorder (tree->right);
+        std::cout<< item ;
+    }
 }
 
 void breadthFirst( Tree tree )
 {
-
-}
+    enqueue(tree);
+    while (!isEmpty(queue))
+    {
+        tree = dequeue();
+        std::cout<<tree ;
+        if (tree->left)
+        {
+            enqueue(tree->left);
+        }
+        else if (tree->right)
+        {
+            enqueue(tree->right);
+        }
+    }
 
 }
 
