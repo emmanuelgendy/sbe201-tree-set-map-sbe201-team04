a830e24a8fb38306c27180f0b5daa76c9b69752b
habiba1997 <habiba.hamad97@eng-st.cu.edu.eg>
2018-04-14 20:26:05 +0200

    Map.hpp Done

--- count_words.cpp
+++ count_words.cpp
@@ -5,18 +5,13 @@
 #include "map.hpp"
 #include "helpers.hpp"
 
-
-int main( int argc, char **argv )
+int main(int argc, char **argv)
 {
-    if( argc == 2 )
+    if (argc == 2)
     {
-        std::vector< std::string > words = getFileWords( argv[1] );
+        std::vector<std::string> words = getFileWords(argv[1]);
 
         // COMPLETE HERE
-
-
-
-
 
 
         // DONE HERE
