7b324d2cd84653c2b0822780085adb16164ea75c
habiba1997 <habiba.hamad97@eng-st.cu.edu.eg>
2018-04-14 21:08:48 +0200

    map.hpp and count_words.cpp Both done

--- count_words.cpp
+++ count_words.cpp
@@ -12,24 +12,27 @@ int main(int argc, char **argv)
     if (argc == 2)
     {
         std::vector<std::string> words = getFileWords(argv[1]);
-        
+
         map::WordMap wMap = map::create();
-        
+
         int sumWordsText = 0;
-        int sumUniqueWords = 0;
-        
+
         for (int i = 0; i < words.size(); ++i)
         {
-            if (!map::contain(wMap, words[i]))
+            if (map::contain(wMap, words[i]))
             {
-                map::insert(wMap, words[i]);
-                sumUniqueWords++;
+                value(wMap, words[i])++;
             }
+            else
+            { 
+                map::insert(wMap, words[i]);
+                value(wMap, words[i]) ++;
+            };
+           
             sumWordsText++;
         }
 
         std::cout << "The Sum of All the Words in the File = " << sumWordsText << std::endl;
-        std::cout << "The Sum of Repeated Words in the File = " << sumUniqueWords << std::endl;
     }
 
     return 0;
